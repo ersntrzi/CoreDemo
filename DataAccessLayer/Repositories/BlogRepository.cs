4b02c9f37376e1c6daa94f0c1b482410f51ead9f
ersntrzi <ersin@basefy.com>
2021-11-30 14:19:14 +0300

    Add project files.

---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrate;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        public void BlogAdd(Blog blog)
+        {
+            using var c = new Context();
+            c.Add(blog);
+            c.SaveChanges();
+        }
+
+        public void DeleteBlog(Blog blog)
+        {
+            using var c = new Context();
+            c.Remove(blog);
+            c.SaveChanges();
+        }
+
+        public Blog GetById(int id)
+        {
+            using var c = new Context();
+            return c.Blogs.Find(id);
+        }
+
+        public List<Blog> ListAllBlog()
+        {
+            using var c = new Context();
+            return c.Blogs.ToList();
+        }
+
+        public void UpdatBlog(Blog blog)
+        {
+            using var c = new Context();
+            c.Update(blog);
+            c.SaveChanges();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        Context c = new Context();
+        public void Add(T item)
+        {
+            c.Add(item);
+            c.SaveChanges();
+        }
+
+        public void Delete(T item)
+        {
+            c.Remove(item);
+            c.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return c.Set<T>().Find(id);
+        }
+
+        public List<T> ListAll()
+        {
+            return c.Set<T>().ToList();
+        }
+
+        public void Update(T item)
+        {
+            c.Update(item);
+            c.SaveChanges();
+        }
+    }
+}
